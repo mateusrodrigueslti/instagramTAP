b5778c0f69958eb0844a1e96d9743c67a49f9c23
Caio Lucena <caiolucenamdr@gmail.com>
2017-06-12 20:07:01 -0300

    primeira tentativa

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,8 +2,8 @@ var mongo = require("mongodb");
 var mongoose = require('mongoose');
 var url = "localhost";
 var port = 27017;
-const connection = mongoose.connect('mongodb://' + url + '/instagram');
-// const connection = mongoose.connect('mongodb://shell:toor@cluster0-shard-00-00-dhpkf.mongodb.net:27017,cluster0-shard-00-01-dhpkf.mongodb.net:27017,cluster0-shard-00-02-dhpkf.mongodb.net:27017/socialmoments?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+//const connection = mongoose.connect('mongodb://' + url + '/instagram');
+const connection = mongoose.connect('mongodb://shell:toor@cluster0-shard-00-00-dhpkf.mongodb.net:27017,cluster0-shard-00-01-dhpkf.mongodb.net:27017,cluster0-shard-00-02-dhpkf.mongodb.net:27017/socialmoments?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
 var connMongoDB = function() {
     return connection;
